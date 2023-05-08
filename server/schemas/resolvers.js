7e7ff84a03f5315d17409995d992f84bd685629c
Dgalus <damiangalus7@gmail.com>
2023-05-08 15:34:09 -0400

    Remove book mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,5 +47,18 @@ const resolvers = {
         }
         throw new AuthenticationError('You have to be logged in');
       },
-    }
-}
+      removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId }}},
+                { new: true },
+            );
+            return updatedUser;
+        }
+        throw new AuthenticationError('You have to be logged in.')
+      },
+    },
+  };
+
+  module.exports = resolvers;

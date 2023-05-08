9c60b8c348340c7aaa0063f7749a67f93f74ad4b
Dgalus <damiangalus7@gmail.com>
2023-05-08 15:29:44 -0400

    add user login, and save book mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,6 +14,38 @@ const resolvers = {
     },
 
     Mutation: {
-      addUser:
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new AuthenticationError('No user found');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect password or email');
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+      },
+      saveBook: async (parent, { newBook }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: newBook }},
+                { new: true },
+            );
+            return updatedUser;
+        }
+        throw new AuthenticationError('You have to be logged in');
+      },
     }
 }

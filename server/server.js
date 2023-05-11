605ac8d18449176fa1526c1d6820c5ed96340356
Dgalus <damiangalus7@gmail.com>
2023-05-10 23:47:27 -0400

    Please work

--- server/server.js
+++ server/server.js
@@ -5,22 +5,21 @@ const { typeDefs, resolvers } = require('./schemas')
 const path = require('path');
 const db = require('./config/connection');
 
-//Set up express server with express() and a port number
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Init apollo server
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 })
 
-//Middleware boilerplate
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
